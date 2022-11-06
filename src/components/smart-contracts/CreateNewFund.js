a7127e2169c7a9137eec64fb69688f4da261de53
sugaim <50139852+sugaim@users.noreply.github.com>
2022-11-06 17:52:16 +0900

    temp

--- src/components/smart-contracts/CreateNewFund.js
+++ src/components/smart-contracts/CreateNewFund.js
@@ -1,6 +1,8 @@
 const createNewFund = ({underlyingAsset, offerClosingTime, orderExpiration, maturity}) => {
     // smart contractの処理書く
-
+    // const fs = require('fs');
+    // const builtContract = JSON.parse(fs.readFileSync('./contracts/CollectiveInvestmentScheme', 'utf8'));
+    // console.log(JSON.stringify(contract.abi));
     return {}
 }
 
